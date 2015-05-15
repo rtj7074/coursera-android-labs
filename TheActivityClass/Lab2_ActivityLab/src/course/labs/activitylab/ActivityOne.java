7ef3523a68c801890c71eb22fb71cb0d4b43c522
Rodlyn Joseph <rodlynjoseph@gmail.com>
2015-05-14 23:57:35 -0400

    Initial Commit before Testing.

--- TheActivityClass/Lab2_ActivityLab/src/course/labs/activitylab/ActivityOne.java
+++ TheActivityClass/Lab2_ActivityLab/src/course/labs/activitylab/ActivityOne.java
@@ -27,6 +27,10 @@ public class ActivityOne extends Activity {
 	// mCreate, mRestart, mStart and mResume
 	// to count calls to onCreate(), onRestart(), onStart() and
 	// onResume(). These variables should not be defined as static.
+	private Integer mCreate=0;
+	private Integer mRestart=0;
+	private Integer mStart=0;
+	private Integer mResume=0;
 
 	// You will need to increment these variables' values when their
 	// corresponding lifecycle methods get called.
@@ -34,6 +38,10 @@ public class ActivityOne extends Activity {
 	// TODO: Create variables for each of the TextViews
 	// named mTvCreate, mTvRestart, mTvStart, mTvResume.
 	// for displaying the current count of each counter variable
+	private TextView mTvCreate;
+	private TextView mTvRestart;
+	private TextView mTvStart;
+	private TextView mTvResume;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +51,10 @@ public class ActivityOne extends Activity {
 		// TODO: Assign the appropriate TextViews to the TextView variables
 		// Hint: Access the TextView by calling Activity's findViewById()
 		// textView1 = (TextView) findViewById(R.id.textView1);
+		mTvCreate  = (TextView) findViewById(R.id.create);
+        mTvRestart = (TextView) findViewById(R.id.restart);
+        mTvResume  = (TextView) findViewById(R.id.resume);
+        mTvStart   = (TextView) findViewById(R.id.start);
 
 		Button launchActivityTwoButton = (Button) findViewById(R.id.bLaunchActivityTwo);
 		launchActivityTwoButton.setOnClickListener(new OnClickListener() {
@@ -68,7 +80,10 @@ public class ActivityOne extends Activity {
 			// TODO:
 			// Restore value of counters from saved state
 			// Only need 4 lines of code, one for every count variable
-
+            mRestart =  savedInstanceState.getInt(RESTART_KEY);
+            mResume  =  savedInstanceState.getInt(RESUME_KEY);
+            mCreate  =  savedInstanceState.getInt(CREATE_KEY);
+            mStart   =  savedInstanceState.getInt(START_KEY);
 		}
 
 		// Emit LogCat message
@@ -77,6 +92,8 @@ public class ActivityOne extends Activity {
 		// TODO:
 		// Update the appropriate count variable
 		// Update the user interface via the displayCounts() method
+        mCreate++;
+        displayCounts();
 
 	}
 
@@ -92,7 +109,8 @@ public class ActivityOne extends Activity {
 		// TODO:
 		// Update the appropriate count variable
 		// Update the user interface
-
+        mStart++;
+        displayCounts();
 	}
 
 	@Override
@@ -105,7 +123,8 @@ public class ActivityOne extends Activity {
 		// TODO:
 		// Update the appropriate count variable
 		// Update the user interface
-
+        mResume++;
+        displayCounts();
 	}
 
 	@Override
@@ -134,7 +153,8 @@ public class ActivityOne extends Activity {
 		// TODO:
 		// Update the appropriate count variable
 		// Update the user interface
-
+        mRestart++;
+        displayCounts();
 	}
 
 	@Override
@@ -150,6 +170,12 @@ public class ActivityOne extends Activity {
 		// TODO:
 		// Save state information with a collection of key-value pairs
 		// 4 lines of code, one for every count variable
+        savedInstanceState.putInt(this.RESTART_KEY,this.mRestart);
+        savedInstanceState.putInt(this.RESUME_KEY,this.mResume);
+        savedInstanceState.putInt(this.CREATE_KEY,this.mCreate);
+        savedInstanceState.putInt(this.START_KEY,this.mStart);
+
+        super.onSaveInstanceState(savedInstanceState);
 
 	}
 
@@ -160,11 +186,11 @@ public class ActivityOne extends Activity {
 	public void displayCounts() {
 
 		// TODO - uncomment these lines
-	/*
+
 		mTvCreate.setText("onCreate() calls: " + mCreate);
 		mTvStart.setText("onStart() calls: " + mStart);
 		mTvResume.setText("onResume() calls: " + mResume);
 		mTvRestart.setText("onRestart() calls: " + mRestart);
-	*/
+
 	}
 }
